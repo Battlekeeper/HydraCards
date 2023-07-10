fdb14e705de6177f737c2589297b5da5469d1099
Battlekeeper <conwaywest@tech-center.com>
2023-07-10 18:43:59 -0400

    socket io setup in backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,29 @@
 import express from 'express';
+import socketio from "socket.io";
+import http from "http";
+import cors from "cors"
+
 
 const app = express();
+app.use(cors({ origin: 'http://localhost:3000' }));
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
   });
 
-app.listen(3001, () =>
-  console.log('Example app listening on port 3001!'),
+const server = http.createServer(app);
+const io = new socketio.Server(server, {cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }});
+
+server.listen(3001, () =>
+  console.log('Backend listening on http://localhost:3001'),
 );
+
+io.on("connection", (socket) => {
+	console.log(socket.id);
+});
+io.on("disconnect", (socket) => {
+	console.log("Disconnected");
+});
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
 import { ref } from "vue"
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:3001");
+socket.on("connect",() => {
+	console.log("Connected")
+})
 
 </script>
 <template>
