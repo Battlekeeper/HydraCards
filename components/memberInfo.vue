504fe917a8978ae64952ffe8007ca7ab8d10901c
Battlekeeper <74582602+Battlekeeper@users.noreply.github.com>
2023-08-09 10:12:43 -0400

    split

--- components/memberInfo.vue
+++ components/memberInfo.vue
@@ -4,7 +4,8 @@
 
 <template>
 	<div class="flex p-10 bg-gray-300 dark:bg-gray-700 rounded-xl gap-4">
-        <p class="text-gray-50 text-5xl font-semibold leading-10">Camden</p>
+        <p class="text-gray-50 text-5xl font-semibold leading-10">User</p>
+        <button></button>
 
     </div>
 </template>
