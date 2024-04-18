714854480fae231dcd5d665746f70fcf38c6da75
PishPish <mrpishpish@gmail.com>
2024-04-18 16:38:48 -0400

    reactive memberinfo buttons

--- components/memberInfo.vue
+++ components/memberInfo.vue
@@ -43,21 +43,27 @@ watch(displayName, ()=>{
 				<p class="text-black dark:text-gray-50 text-5xl font-semibold leading-10">{{ props.member.displayName }}</p>
 			</div>
 			<div class="flex flex-row">
-				<button @click="kick()" class="bg-blue-800 dark:bg-orange-500 text-white rounded-md p-3 mr-4">Kick
-					Player</button>
+				<button @click="kick()" 
+					class="bg-blue-800 dark:bg-orange-500 text-white rounded-md p-3 mr-4 border border-blue-800 dark:border-orange-500 hover:text-blue-800 dark:hover:text-orange-500 hover:bg-white dark:hover:bg-white">Kick
+					Player
+				</button>
 				<button v-if="props.member.permissions.host" @click="promoteToModerator(false)"
-					class="bg-transparent dark:text-orange-500 border p-3 dark:border-orange-500 border-blue-800 text-blue-800 rounded-md">Demote from Moderator</button>
+					class="bg-transparent dark:text-orange-500 border p-3 dark:border-orange-500 border-blue-800 text-blue-800 rounded-md hover:text-white dark:hover:text-white hover:bg-blue-800 dark:hover:bg-orange-500">
+					Demote from Moderator
+				</button>
 				<button v-if="!props.member.permissions.host" @click="promoteToModerator(true)"
-					class="bg-transparent dark:text-orange-500 border p-3 dark:border-orange-500 border-blue-800 text-blue-800 rounded-md">Promote
-					to Moderator</button>
+					class="bg-transparent dark:text-orange-500 border p-3 dark:border-orange-500 border-blue-800 text-blue-800 rounded-md hover:text-white dark:hover:text-white hover:bg-blue-800 dark:hover:bg-orange-500">
+					Promote to Moderator
+				</button>
 			</div>
 		</div>
 		<div v-if="props.member.id == currentUser.id" class="flex pb-10 pl-10 pr-10">
 			<input v-model="displayName" text="text"
 				class="w-80 mt-4 mr-2 bg-white dark:bg-gray-300 font-normal p-2 rounded-md text-black"
 				placeholder="Enter Your Name">
-			<button @click="apiSetDisplayName()"
-			class="w-fit h-fit bg-transparent text-blue-800 dark:text-orange-500 px-2 py-2 mt-4 flex self-center rounded-md border border-blue-800 dark:border-orange-500 font-medium">Set
-			Name</button>
+			<button @click="apiSetDisplayName()" 
+				class="w-fit h-fit bg-transparent text-blue-800 dark:text-orange-500 px-2 py-2 mt-4 flex self-center rounded-md border border-blue-800 dark:border-orange-500 font-medium hover:text-white hover:dark:text-white hover:bg-blue-800 hover:dark:bg-orange-500">
+				Set Name
+			</button>
 	</div>
 </div></template>
