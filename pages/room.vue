af7d5f6b3b02790340de4455bf158b5bf0ec6b2d
PishPish <mrpishpish@gmail.com>
2024-05-16 16:06:25 -0400

    permanent header css updates + sign in start

--- pages/room.vue
+++ pages/room.vue
@@ -424,7 +424,7 @@ watch(roomTopicName, ()=>{
 	<div v-if="currentUser.permissions.host && currentRoom.status == HCRoomStatus.voting" class="flex flex-col lg:flex-row-reverse justify-center lg:gap-20 gap-10 mt-7 mx-7">
 		<div class="flex flex-col lg:max-w-xl gap-5 xl:mr-8">
 			<div class="flex justify-center items-center gap-5">
-				<input v-model="roomTopicName" placeholder="Enter Story Name" class="w-full bg-gray-300 dark:bg-gray-700 rounded-2xl p-4">
+				<input v-on:keyup.enter="socketSetTopicName()" v-model="roomTopicName" placeholder="Enter Story Name" class="w-full bg-gray-300 dark:bg-gray-700 rounded-2xl p-4">
 				<svg v-if="currentRoom.topicName != roomTopicName" @click="socketSetTopicName()" mlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-floppy-fill cursor-pointer" viewBox="0 0 16 16">
 					<path d="M0 1.5A1.5 1.5 0 0 1 1.5 0H3v5.5A1.5 1.5 0 0 0 4.5 7h7A1.5 1.5 0 0 0 13 5.5V0h.086a1.5 1.5 0 0 1 1.06.44l1.415 1.414A1.5 1.5 0 0 1 16 2.914V14.5a1.5 1.5 0 0 1-1.5 1.5H14v-5.5A1.5 1.5 0 0 0 12.5 9h-9A1.5 1.5 0 0 0 2 10.5V16h-.5A1.5 1.5 0 0 1 0 14.5z"/>
 					<path d="M3 16h10v-5.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5zm9-16H4v5.5a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 .5-.5zM9 1h2v4H9z"/>
